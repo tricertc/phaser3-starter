99742f68dd8e795000aaa1a6630428c3b1edd795
Tim Rice <timrice@gmail.com>
2018-12-05 10:24:58 -0500

    hmr support

--- src/game.ts
+++ src/game.ts
@@ -4,9 +4,25 @@ import MainScene from './scenes/MainScene'
 
 const game = new Phaser.Game({
   type: Phaser.AUTO,
+  parent: 'game',
   width: 800,
   height: 600,
   scene: [
     MainScene
   ]
 })
+
+declare var module: {
+  hot: {
+    dispose(callback: () => void): void
+  }
+}
+
+if (module.hot) {
+  const gameEl = document.getElementById(game.config.parent)
+  module.hot.dispose(() => {
+    while (gameEl && gameEl.firstChild) {
+      gameEl.removeChild(gameEl.firstChild)
+    }
+  })
+}
